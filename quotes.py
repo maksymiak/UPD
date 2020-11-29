839f5f7dbb12161e2b6619c679912eac73ae8fcd
maksymiak <61247352+maksymiak@users.noreply.github.com>
2020-11-29 14:49:10 +0100

    Update quotes.py

--- quotes.py
+++ quotes.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] ='postgresql+psycopg2://pmaksymiak:Ecg61str8!@db-fav-quotes.postgres.database.azure.com/db-fav-quotes'
+app.config['SQLALCHEMY_DATABASE_URI'] ='postgresql+psycopg2://pmaksymiak@db-fav-quotes:Ecg61str8!@db-fav-quotes.postgres.database.azure.com:5432/db-fav-quotes'
 #app.config['SQLALCHEMY_DATABASE_URI'] ='postgres://ylncrmosvvrvif:71441e668e08b0b5bda470a3673e6e879f94466b5e7865eb9ad18268c1f807c3@ec2-54-75-231-215.eu-west-1.compute.amazonaws.com:5432/d7aofqpvufo2mq'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
 
